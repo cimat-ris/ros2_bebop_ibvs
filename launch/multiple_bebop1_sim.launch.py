06d8e65ec622e7f0c51a092908e627b730490606
bloodfield <edgar.chavez@cimat.mx>
2025-12-17 13:11:11 -0600

    Multiple spawn

---
+++ launch/multiple_bebop1_sim.launch.py
@@ -0,0 +1,95 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import os
+import yaml
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Obtener rutas de los paquetes
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    pkg_bebop_ibvs = get_package_share_directory('ros2_bebop_ibvs')
+
+
+    # Lanzar Gazebo
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={
+            'gz_args': '-r -z 1000000 simple_lab_2_flat.world ',
+        'on_exit_shutdown': 'true'}.items(),
+    )
+
+
+
+    # Lanzar el puente ROS-Gazebo
+    bridge = []
+    models = []
+    for i in range(4):
+        ros_gz_bridge = Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='ros_gz_bridge_'+str(i),
+            output='screen',
+            parameters=[{
+                'expand_gz_topic_names': True,  # Activate the expand_gz_topic_names parameter
+                'config_file': os.path.join(pkg_bebop_ibvs, 'config', 'bebop1.yaml'),
+            }],
+        )
+        bridge.append(ros_gz_bridge)
+
+
+        #   TODO: Loop
+        #   Spawn bebop
+        bebop_model = os.path.join(pkg_bebop_ibvs,"models","parrot_bebop_2","model.sdf")
+        bebop_spawn = Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=[
+                '-name', 'parrot_bebop_2_'+str(i),
+                '-world', 'default',
+                '-file', bebop_model,
+                '-x', str(-i),
+                '-y', '-1.',
+                '-z', '0.1',
+                '-Y', '1.',
+            ],
+            output='screen',
+        )
+        models.append(bebop_spawn)
+
+
+    # #   IBFC
+    # yaml_control = os.path.join(pkg_bebop_ibvs, 'config', 'control_sim.yaml')
+    # with open(yaml_control, 'r') as file:
+    #     config = yaml.safe_load(file)
+    # # config["ref_image"] = os.path.join(pkg_bebop_ibvs, 'config', 'reference_4_2.png')
+    # config["ref_image"] = os.path.join(pkg_bebop_ibvs, 'config', 'reference_2_flat.png')
+    # # config["ref_image"] = os.path.join(pkg_bebop_ibvs, 'config', 'reference_1_aruco.png')
+    # if not os.path.exists(config["ref_image"]):
+    #     print(f"Camara reference {config["ref_image"]} does not exists")
+    #     return
+    #
+    # if not os.path.exists(config["output"]):
+    #     print(f"Output directory  {config["output"]} does not exists")
+    #     return
+    #
+    # controller = Node(
+    #         package='ros2_bebop_ibvs',
+    #         executable='ibvs_sim',
+    #         name='ibvs',
+    #         output='screen',
+    #         parameters=[config]
+    #     )
+
+    _des = [
+        gz_sim,
+        # bebop_spawn,
+        # ros_gz_bridge,
+        # controller,
+    ]
+
+    return LaunchDescription(_des + bridge + models)
